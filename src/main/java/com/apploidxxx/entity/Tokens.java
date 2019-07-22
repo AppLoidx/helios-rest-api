1b9811a92f2be98db3b2abd1962eedcd3dea7852
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-22 21:41:31 +0900

    added serialization properties aliases for variables

--- src/main/java/com/apploidxxx/entity/Tokens.java
+++ src/main/java/com/apploidxxx/entity/Tokens.java
@@ -1,14 +1,26 @@
 package com.apploidxxx.entity;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbTransient;
 
 /**
  * @author Arthur Kupriyanov
  */
 public class Tokens {
+
+    @JsonAlias("token")
+    @JsonbProperty("token")
     public final String token;
+
     @JsonAlias("refresh_token")
+    @JsonbProperty("refresh_token")
     public final String refreshToken;
+
+    @JsonIgnore
+    @JsonbTransient
     public final User user;
 
     public Tokens(String token, String refreshToken, User user){
