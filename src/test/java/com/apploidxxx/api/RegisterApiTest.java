891407faba39ecc5c4859ccf72d742fd73cb3e65
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-29 14:44:08 +0900

    code review

--- src/test/java/com/apploidxxx/api/RegisterApiTest.java
+++ src/test/java/com/apploidxxx/api/RegisterApiTest.java
@@ -16,28 +16,17 @@ import static io.restassured.RestAssured.post;
  */
 public class RegisterApiTest {
     private HttpServer server;
-    private String username_123_register = Main.BASE_URI + "api/register?username=123&password=123&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
-    private String username_123_delete = Main.BASE_URI + "api/register?username=123&password=123";
-    private String username_321_register = Main.BASE_URI + "api/register?username=321&password=123&first_name=Fedor&last_name=Bashkirov&email=testEmail@gmail.com";
-    private String username_321_delete = Main.BASE_URI + "api/register?username=321&password=123";
 
-    private String username_register_without_username = Main.BASE_URI + "api/register?password=123&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
-    private String username_111_register_without_email = Main.BASE_URI + "api/register?username=111&password=123&first_name=Arthur&last_name=Kupriyanov";
-    private String username_222_register_without_password = Main.BASE_URI + "api/register?username=222&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
-    private String username_333_register_without_name = Main.BASE_URI + "api/register?username=333&password=123&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
-    private String username_444_register_without_lastname = Main.BASE_URI + "api/register?username=444&password=123&first_name=Arthur&email=apploidyakutsk@gmail.com";
-
-    public String deleteUserQuery(String username, String password){
-        return String.format(Main.BASE_URI + "api/register?username=%s&password=%s",username, password);
-    }
-    public String deleteUserQuery(String username){
-        return deleteUserQuery(username, "123");
+    private String deleteUserQuery(String username){
+        return String.format(Main.BASE_URI + "api/register?username=%s&password=%s",username, "123");
     }
 
     @Before
     public void setUp() throws Exception {
         this.server = Main.startServer();
+        String username_123_delete = Main.BASE_URI + "api/register?username=123&password=123";
         delete(username_123_delete);
+        String username_321_delete = Main.BASE_URI + "api/register?username=321&password=123";
         delete(username_321_delete);
 
         delete(deleteUserQuery("111"));
@@ -48,15 +37,27 @@ public class RegisterApiTest {
 
     @Test
     public void create_user_and_delete_response_successful(){
+        String username_123_register = Main.BASE_URI + "api/register?username=123&password=123&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
         post(username_123_register).then().statusCode(200);
         post(username_123_register).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        String username_321_register = Main.BASE_URI + "api/register?username=321&password=123&first_name=Fedor&last_name=Bashkirov&email=testEmail@gmail.com";
         post(username_321_register).then().statusCode(200);
 
-        post(username_register_without_username).then().statusCode(400);
-        post(username_111_register_without_email).then().statusCode(400);
-        post(username_222_register_without_password).then().statusCode(400);
-        post(username_333_register_without_name).then().statusCode(400);
-        post(username_444_register_without_lastname).then().statusCode(400);
+        String username_register_without_username = Main.BASE_URI + "api/register?password=123&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
+        post(username_register_without_username).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        String username_111_register_without_email = Main.BASE_URI + "api/register?username=111&password=123&first_name=Arthur&last_name=Kupriyanov";
+        post(username_111_register_without_email).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        String username_222_register_without_password = Main.BASE_URI + "api/register?username=222&first_name=Arthur&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
+        post(username_222_register_without_password).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        String username_333_register_without_name = Main.BASE_URI + "api/register?username=333&password=123&last_name=Kupriyanov&email=apploidyakutsk@gmail.com";
+        post(username_333_register_without_name).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        String username_444_register_without_lastname = Main.BASE_URI + "api/register?username=444&password=123&first_name=Arthur&email=apploidyakutsk@gmail.com";
+        post(username_444_register_without_lastname).then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
 
     }
 
