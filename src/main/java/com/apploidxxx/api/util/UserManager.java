30f67921aeb61664e347b2a1977b288e421cdc98
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-31 00:43:08 +0900

    added getUserByName method and renamed

--- src/main/java/com/apploidxxx/api/util/UserManager.java
+++ src/main/java/com/apploidxxx/api/util/UserManager.java
@@ -2,14 +2,24 @@ package com.apploidxxx.api.util;
 
 import com.apploidxxx.api.exceptions.InvalidTokenException;
 import com.apploidxxx.entity.User;
+import com.apploidxxx.entity.dao.user.UserService;
 
 /**
  * @author Arthur Kupriyanov
  */
-public class UserSessionManager {
+public class UserManager {
     public static User getUser(String token) throws InvalidTokenException {
         User user = UserSession.getUser(token);
         if (user == null) {
+            throw new InvalidTokenException();
+        } else {
+            return user;
+        }
+    }
+
+    public static User getUserByName(String name) throws InvalidTokenException {
+        User user = UserService.findByName(name);
+        if (user == null){
             throw new InvalidTokenException();
         } else {
             return user;
