6722c46a00b424629834319c8ed255eaa6a8e6ab
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-31 23:25:57 +0900

    added type set tests

--- src/test/java/com/apploidxxx/api/QueueControlApiTest.java
+++ src/test/java/com/apploidxxx/api/QueueControlApiTest.java
@@ -6,7 +6,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.core.Response;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
 
 /**
  * @author Arthur Kupriyanov
@@ -46,14 +50,34 @@ public class QueueControlApiTest {
         put(Main.BASE_URI + "api/queue?queue_name=testQue&access_token=" + token);
         put(Main.BASE_URI + "api/queue?queue_name=testQue&access_token=" + anotherUserToken);
 
-        System.out.println(get(Main.BASE_URI + "api/queue?queue_name=testQue").then().extract().body().jsonPath().getList("queueSequence"));
-
         put(Main.BASE_URI + path + "testQue?action=shuffle&access_token=" + token).then().statusCode(200);
         get(Main.BASE_URI + "api/queue?queue_name=testQue").then().statusCode(200);
 
         delete(Main.BASE_URI + "api/queue?access_token="+token+"&queue_name=testQue&target=QUEUE").then().statusCode(200);
     }
 
+    @Test
+    public void test_type_change_with_correct_type(){
+        post(Main.BASE_URI + "api/queue?access_token="+token+"&queue_name=testQue&fullname=Test%20Queue")
+                .then().statusCode(200);
+        put(Main.BASE_URI + path + "testQue?action=setType&type=one_week&access_token=" + token).then().statusCode(200);
+        get(Main.BASE_URI + "api/queue?queue_name=testQue").then().body("generation_type", equalTo("ONE_WEEK")).and().statusCode(200);
+
+        delete(Main.BASE_URI + "api/queue?access_token="+token+"&queue_name=testQue&target=QUEUE").then().statusCode(200);
+    }
+
+    @Test
+    public void test_type_change_with_incorrect_type(){
+        post(Main.BASE_URI + "api/queue?access_token="+token+"&queue_name=testQue&fullname=Test%20Queue")
+                .then().statusCode(200);
+        put(Main.BASE_URI + path + "testQue?action=setType&type=incorrect_type&access_token=" + token)
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .and()
+                .body("error", equalTo("invalid_param"));
+        get(Main.BASE_URI + "api/queue?queue_name=testQue").then().body("generation_type", nullValue()).and().statusCode(200);
+        delete(Main.BASE_URI + "api/queue?access_token="+token+"&queue_name=testQue&target=QUEUE").then().statusCode(200);
+    }
 
     @After
     public void tearDown() throws Exception {
