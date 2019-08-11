6434ac27d8121c329b46fc8b1f07d86867de6d25
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-11 10:45:12 +0900

    added queues member property

--- src/main/java/com/apploidxxx/api/model/UserInfo.java
+++ src/main/java/com/apploidxxx/api/model/UserInfo.java
@@ -2,7 +2,10 @@ package com.apploidxxx.api.model;
 
 import com.apploidxxx.entity.User;
 import com.apploidxxx.entity.queue.Queue;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+import javax.json.bind.annotation.JsonbProperty;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -12,25 +15,35 @@ import java.util.Set;
 /**
  * @author Arthur Kupriyanov
  */
+@Data
 public class UserInfo implements Serializable {
     private  User user;
     private  List<String[]> queues;
+    @JsonProperty("queues_member")
+    @JsonbProperty("queues_member")
+    private  List<String[]> queuesMember;
 
     public UserInfo(User user){
         this.user = user;
-        queues = getQueues(user);
+        setQueues(user);
     }
 
-    private List<String[]> getQueues(User user){
-        Set<String[]> list = new HashSet<>();
-        Set<Queue> set = new HashSet<>();
-        set.addAll(user.getQueueMember());
-        set.addAll(user.getQueueSuper());
-        for (Queue q: set
-             ) {
-            list.add(new String[]{q.getName(), q.getFullname()});
+    private void setQueues(User user){
+        Set<String[]> AllList = new HashSet<>();
+        Set<String[]> memberList = new HashSet<>();
+        Set<Queue> memberSet = new HashSet<>(user.getQueueMember());
+        Set<Queue> superSet = new HashSet<>(user.getQueueSuper());
+        for (Queue q: memberSet
+        ) {
+            memberList.add(new String[]{q.getName(), q.getFullname()});
+            AllList.add(new String[]{q.getName(), q.getFullname()});
         }
-        return new ArrayList<>(list);
+        for (Queue q: superSet
+             ) {
+            AllList.add(new String[]{q.getName(), q.getFullname()});
+        }
+        this.queues = new ArrayList<>(AllList);
+        this.queuesMember = new ArrayList<>(memberList);
     }
 
     public User getUser() {
