376af2d71312c561acdd9bf1488e027648c2bca7
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-21 15:02:59 +0900

    members property set for sorted by sequence array

--- src/main/java/com/apploidxxx/entity/queue/Queue.java
+++ src/main/java/com/apploidxxx/entity/queue/Queue.java
@@ -64,6 +64,8 @@ public class Queue implements Serializable {
     @JoinTable(name="QUEUE_MEMBERS",
             joinColumns = {@JoinColumn(name="queue_name")},
             inverseJoinColumns={@JoinColumn(name="users_id")})
+    @JsonIgnore
+    @JsonbTransient
     private Set<User> members;
 
     @JsonProperty("super_users")
@@ -127,8 +129,8 @@ public class Queue implements Serializable {
         QueueService.updateQueue(this);
     }
 
-    @JsonIgnore
-    @JsonbTransient
+    @JsonbProperty("members")
+    @JsonProperty("members")
     public List<User> getMembersList() {
         Map<Long, User> mapping = new HashMap<>();
         for (User u : members){
