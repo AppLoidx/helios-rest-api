c203e3e33b596fe148f7c2f46747175ce81dfd99
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-28 18:48:25 +0900

    added group param for register

--- src/main/java/com/apploidxxx/api/RegisterApi.java
+++ src/main/java/com/apploidxxx/api/RegisterApi.java
@@ -4,10 +4,12 @@ import com.apploidxxx.Main;
 import com.apploidxxx.api.exceptions.VulnerabilityException;
 import com.apploidxxx.api.model.ErrorMessage;
 import com.apploidxxx.api.util.ErrorResponseFactory;
+import com.apploidxxx.api.util.GroupChecker;
 import com.apploidxxx.api.util.Password;
 import com.apploidxxx.api.util.VulnerabilityChecker;
 import com.apploidxxx.entity.User;
 import com.apploidxxx.entity.dao.user.UserService;
+import org.jboss.logging.Logger;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -22,39 +24,60 @@ import javax.ws.rs.core.Response;
  */
 @Path("/api/register")
 public class RegisterApi {
+    private final Logger logger = Logger.getLogger(RegisterApi.class);
+
     @POST
     public Response register(@Valid@NotNull@QueryParam("username") String username,
                              @Valid@NotNull@QueryParam("password") String password,
                              @Valid@NotNull@QueryParam("first_name") String firstName,
                              @Valid@NotNull@QueryParam("last_name") String lastName,
-                             @Valid@NotNull@QueryParam("email") String email){
+                             @Valid@NotNull@QueryParam("email") String email,
+                             @Valid@QueryParam("group") String group){
+        logger.info(String.format("Params \nusername: %s,\npassword: %s, \nfirst_name: %s,\nlast_name: %s\nemail: %s,\ngroup: %s",
+                username, password, firstName, lastName, email, group));
 
+        logger.info("Validating the password");
         if ("".equals(password) || (password.length() < 8 && Main.validatePassword)){
             return ErrorResponseFactory.getInvalidParamErrorResponse("Your password length is too small");
         }
 
+        logger.info("Validating the email");
         if (!email.matches(".+@.+")){
             return ErrorResponseFactory.getInvalidParamErrorResponse("Invalid email param");
         }
 
+        logger.info("Validating first_name param");
         if (!firstName.matches("[^\\s]+") || !lastName.matches("[^\\s]+")){
             return ErrorResponseFactory.getInvalidParamErrorResponse("Invalid first_name or last_name param");
         }
 
+        logger.info("Validating last_name param");
         if (!username.matches("[^\\s]+")){
             return ErrorResponseFactory.getInvalidParamErrorResponse("Invalid username param");
         }
+
+        logger.info("Validating group");
+        if (group != null && !group.matches("[^\\s]+")){
+            return ErrorResponseFactory.getInvalidParamErrorResponse("Invalid group name");
+        } else {
+            if (group != null && !GroupChecker.isValid(group)) return ErrorResponseFactory.getInvalidParamErrorResponse("Group not found");
+        }
+
+        logger.info("Checking params vulnerabilities");
 
         try {
             VulnerabilityChecker.checkWord(firstName);
             VulnerabilityChecker.checkWord(lastName);
             VulnerabilityChecker.checkWord(username);
+            if (group != null) VulnerabilityChecker.checkWord(group);
         } catch (VulnerabilityException e) {
-            e.printStackTrace();
+            logger.info("Founded exception", e);
+            return e.getResponse();
         }
 
         if (UserService.findByName(username)==null){
-            UserService.saveUser(new User(username, Password.hash(password), firstName, lastName, email));
+            logger.info("Saving new user");
+            UserService.saveUser(new User(username, Password.hash(password), firstName, lastName, email, group));
             return Response.ok().build();
         }
         else {
---
+++ src/main/java/com/apploidxxx/api/util/GroupChecker.java
@@ -0,0 +1,11 @@
+package com.apploidxxx.api.util;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class GroupChecker {
+    public static boolean isValid(String group){
+        return true;
+    }
+
+}
