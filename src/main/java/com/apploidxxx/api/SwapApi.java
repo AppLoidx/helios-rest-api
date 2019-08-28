768e1f1b3daf509c42ee8073b71d5d77be9bd638
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-28 18:48:41 +0900

    added swap api

---
+++ src/main/java/com/apploidxxx/api/SwapApi.java
@@ -0,0 +1,49 @@
+package com.apploidxxx.api;
+
+import com.apploidxxx.api.exceptions.ResponsibleException;
+import com.apploidxxx.api.model.ErrorMessage;
+import com.apploidxxx.api.util.QueueManager;
+import com.apploidxxx.api.util.UserManager;
+import com.apploidxxx.entity.User;
+import com.apploidxxx.entity.dao.queue.QueueService;
+import com.apploidxxx.entity.queue.Queue;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@Path("/api/swap")
+public class SwapApi {
+    @POST
+    public Response requestSwap(@Valid@NotNull@QueryParam("access_token") String accessToken,
+                                @Valid@NotNull@QueryParam("target") String targetUsername,
+                                @Valid@NotNull@QueryParam("queue_name") String queueName){
+        User user;
+        User targetUser;
+        Queue queue;
+        try {
+            user = UserManager.getUser(accessToken);
+            targetUser = UserManager.getUserByName(targetUsername);
+            queue = QueueManager.getQueue(queueName);
+        } catch (ResponsibleException e) {
+            return e.getResponse();
+        }
+
+        if (!queue.getMembers().contains(user) || !queue.getMembers().contains(targetUser)){
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorMessage("user_not_found", "User not found in requested queue")).build();
+        }
+
+        queue.getSwapContainer().addSwapRequest(user , targetUser);
+        QueueService.updateQueue(queue);
+
+
+        return Response.ok().build();
+    }
+}
