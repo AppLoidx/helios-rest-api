5f73e84cb5d87699b04422bc7e2cf900145e9746
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-09-14 18:02:58 +0300

    splted local and server run classes

---
+++ src/main/java/com/apploidxxx/HerokuMain.java
@@ -0,0 +1,58 @@
+package com.apploidxxx;
+
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class HerokuMain {
+
+    // Getting port from JAVA_OPTS
+    private static String port = System.getenv("PORT");
+    // HEROKU URI Setting up
+    private static final String BASE_URI_HEROKU = "http://0.0.0.0:" + port + "/";
+
+
+
+    private static URI getBaseURI(int port) {
+        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+    }
+
+    /**
+     * Starts Heroku Grizzly HTTP server
+     * @return Grizzly HTTP server
+     */
+    private static HttpServer startServerHeroku(URI uri) {
+
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.apploidxxx package
+        final ResourceConfig rc = new ResourceConfig().packages("com.apploidxxx");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(uri, rc);
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = startServerHeroku(getBaseURI(Integer.parseInt(System.getenv("PORT"))));
+
+
+        CLStaticHttpHandler handler = new CLStaticHttpHandler(Main.class.getClassLoader(), "/static/");
+        server.getServerConfiguration().addHttpHandler(handler, "/html/");
+
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI_HEROKU));
+        while(true) {
+            System.in.read();
+        }
+    }
+}
--- src/main/java/com/apploidxxx/Main.java
+++ src/main/java/com/apploidxxx/Main.java
@@ -1,10 +1,10 @@
 package com.apploidxxx;
 
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.util.logging.ConsoleHandler;
@@ -19,10 +19,7 @@ public class Main {
     // Base URI the Grizzly HTTP server for tests will listen on
     public static final String BASE_URI = "http://localhost:3000/";
     public static boolean validatePassword = true;
-    // Getting port from JAVA_OPTS
-    private static String port = System.getenv("PORT");
-    // HEROKU URI Setting up
-    private static final String BASE_URI_HEROKU = "http://0.0.0.0:" + port + "/";
+
 
     /**
      * Starts local Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -39,26 +36,6 @@ public class Main {
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
-
-    private static URI getBaseURI(int port) {
-        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
-    }
-
-    /**
-     * Starts Heroku Grizzly HTTP server
-     * @return Grizzly HTTP server
-     */
-    private static HttpServer startServerHeroku(URI uri) {
-
-        setUpLogger();
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.apploidxxx package
-        final ResourceConfig rc = new ResourceConfig().packages("com.apploidxxx");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(uri, rc);
     }
 
     private static void setUpLogger(){
@@ -80,14 +57,14 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-//        startServerHeroku(getBaseURI(Integer.parseInt(System.getenv("PORT"))));
-            startServer();
-//        // adding static files mapping
-//        CLStaticHttpHandler handler = new CLStaticHttpHandler(Main.class.getClassLoader(), "/static/");
-//        server.getServerConfiguration().addHttpHandler(handler, "/html");
+
+        HttpServer server = startServer();
+
+        CLStaticHttpHandler handler = new CLStaticHttpHandler(Main.class.getClassLoader(), "/static/");
+        server.getServerConfiguration().addHttpHandler(handler, "/html/");
 
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI_HEROKU));
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         while(true) {
             System.in.read();
         }
